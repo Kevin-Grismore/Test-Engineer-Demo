0f81a1cb9c683d591fcad95c44b6bfef8097116b
Kevin-Grismore <kevin.grismore@gmail.com>
2020-12-27 15:29:47 -0500

    Iniital commit

---
+++ Games.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Games.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
