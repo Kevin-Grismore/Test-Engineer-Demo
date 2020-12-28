c3584df10b58aebc7365f310e509afbcf83e0fd5
Kevin-Grismore <kevin.grismore@gmail.com>
2020-12-28 13:16:11 -0500

    Added input field validation

--- RazorPagesGame/Models/Game.cs
+++ RazorPagesGame/Models/Game.cs
@@ -7,16 +7,29 @@ namespace RazorPagesGame.Models
     public class Game
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        //Regex match: Starts with at least one capital letter, ends with 0 more letters 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
         public string Genre { get; set; }
 
-        [Column(TypeName = "decimal(18,2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
+        //Regex match: Starts with at least one capital letter, ends with 0 more letters, nubmers, quotes, or white space
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
