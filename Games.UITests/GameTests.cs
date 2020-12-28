c23465a529301e9256326afe2233a65edac16dfd
Kevin-Grismore <kevin.grismore@gmail.com>
2020-12-28 18:57:45 -0500

    Added first selenium test

---
+++ Games.UITests/GameTests.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Games.UITests
+{
+    public class GameTests
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver(Path.GetFullPath(@"../../../../" + "_drivers"));
+        }
+
+        // 1. Go to Games page
+        // 2. Assert Uncharted is displayed
+        [Test]
+        public void UnchartedIsOnGamesPage()
+        {
+            driver.Url = "https://localhost:5001/games";
+            var uncharted = driver.FindElement(By.CssSelector("td[id='Uncharted']"));
+            Assert.That(uncharted.Displayed);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
